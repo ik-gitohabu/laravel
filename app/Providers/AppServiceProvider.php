c370144efd79c91f6193419be63e58992fd4f948
ik-gitohabu <ik-elgoog@gmail.com>
2019-01-29 16:07:01 +0900

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,9 +11,14 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
+
+//    use Illuminate\Support\Facades\Schema;
+
     public function boot()
     {
         //
+//        Schema::defaultStringLength(191);
+	\Illuminate\Support\Facades\Schema::defaultStringLength(191);
     }
 
     /**
